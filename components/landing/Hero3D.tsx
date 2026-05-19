64fc574a200e00b43f14bd0313ec85dbf4ea7b21
utayacuchooficial <121044960+Anthonny-wt@users.noreply.github.com>
2026-05-19 11:33:39 -0500

    Lista para producción
    Lista para producción

--- components/landing/Hero3D.tsx
+++ components/landing/Hero3D.tsx
@@ -3,10 +3,11 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial } from '@react-three/drei'
+// @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm'
 
 function StarBackground(props: any) {
-  const ref = useRef<any>()
+  const ref = useRef<any>(null)
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.5 })
 
   useFrame((state, delta) => {
